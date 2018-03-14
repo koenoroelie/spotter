4e94c4cf563f0f3e948d01d531031baf1ddf01ca
koenoroelie <koenvanderbaan@hotmail.com>
2018-03-14 20:31:24 +0100

    Finish login page styling. Fix routing issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   end
 
-  def home
+  def dashboard
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#login'
   resource :user
+
+  get 'users/dashboard'
 end
